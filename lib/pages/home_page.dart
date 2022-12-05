7b07088d45347e2621331462fe69c1cc5cc54f2b
Aldairtunq <201912945b@utea.edu.pe>
2022-12-05 12:12:03 -0500

    video 6 completo

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  CollectionReference taskReference =
+      FirebaseFirestore.instance.collection('task');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Firebase Firestore"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                taskReference.get().then((QuerySnapshot value) {
+                  // QuerySnapshot collection = value;
+                  // List<QueryDocumentSnapshot> docs = collection.docs;
+                  // QueryDocumentSnapshot doc = docs[1];
+                  // print(doc.id);
+                  // print(doc.data());
+
+                  QuerySnapshot collection = value;
+                  collection.docs.forEach((QueryDocumentSnapshot element) {
+                    Map<String, dynamic> myMap =
+                        element.data() as Map<String, dynamic>;
+                    print(myMap["title"]);
+                  });
+                });
+              },
+              child: Text("obtener la data desde fires"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
