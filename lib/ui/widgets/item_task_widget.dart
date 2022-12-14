aa132bfeaaa0f7930a5a0254da9b089985cbac38
Aldairtunq <201912945b@utea.edu.pe>
2022-12-14 11:18:28 -0500

    video 22 completo

--- lib/ui/widgets/item_task_widget.dart
+++ lib/ui/widgets/item_task_widget.dart
@@ -12,53 +12,97 @@ class ItemTaskWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 16.0),
-      margin: const EdgeInsets.symmetric(vertical: 8.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(14.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.05),
-            offset: const Offset(4, 4),
-            blurRadius: 12.0,
-          )
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ItemCategoryWidget(
-            text: taskModel.category,
-          ),
-          divider3(),
-          Text(
-            taskModel.title,
-            style: TextStyle(
-              fontSize: 15.0,
-              fontWeight: FontWeight.w600,
-              color: kBrandPrimaryColor.withOpacity(0.85),
+        padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 16.0),
+        margin: const EdgeInsets.symmetric(vertical: 8.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              offset: const Offset(4, 4),
+              blurRadius: 12.0,
+            )
+          ],
+        ),
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ItemCategoryWidget(
+                  text: taskModel.category,
+                ),
+                divider3(),
+                Text(
+                  taskModel.title,
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w600,
+                    color: kBrandPrimaryColor.withOpacity(0.85),
+                  ),
+                ),
+                Text(
+                  taskModel.description,
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w500,
+                    color: kBrandPrimaryColor.withOpacity(0.75),
+                  ),
+                ),
+                divider6(),
+                Text(
+                  taskModel.date,
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w600,
+                    color: kBrandPrimaryColor.withOpacity(0.75),
+                  ),
+                ),
+              ],
             ),
-          ),
-          Text(
-            taskModel.description,
-            style: TextStyle(
-              fontSize: 14.0,
-              fontWeight: FontWeight.w500,
-              color: kBrandPrimaryColor.withOpacity(0.75),
+            Positioned(
+              top: -10,
+              right: -12,
+              child: PopupMenuButton(
+                  elevation: 2,
+                  color: Colors.white,
+                  icon: Icon(
+                    Icons.more_vert,
+                    color: kBrandPrimaryColor.withOpacity(0.85),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(14.0),
+                  ),
+                  onSelected: (value) {
+                    print(value);
+                  },
+                  itemBuilder: (BuildContext context) {
+                    return [
+                      PopupMenuItem(
+                        value: "hola",
+                        child: Text(
+                          "Editar",
+                          style: TextStyle(
+                            fontSize: 13.0,
+                            color: kBrandPrimaryColor.withOpacity(0.85),
+                          ),
+                        ),
+                      ),
+                      PopupMenuItem(
+                        value: "hola2",
+                        child: Text(
+                          "finalizar",
+                          style: TextStyle(
+                            fontSize: 13.0,
+                            color: kBrandPrimaryColor.withOpacity(0.85),
+                          ),
+                        ),
+                      ),
+                    ];
+                  }),
             ),
-          ),
-          divider6(),
-          Text(
-            taskModel.date,
-            style: TextStyle(
-              fontSize: 14.0,
-              fontWeight: FontWeight.w600,
-              color: kBrandPrimaryColor.withOpacity(0.75),
-            ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
